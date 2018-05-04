950ce0e194fffd61d13178984c87a86089bfeeee
ortal <ortal@appcard.com>
2018-05-04 21:17:34 +0300

    Fixed Client to look more normal code

--- client.py
+++ client.py
@@ -1,24 +1,20 @@
 import subprocess
 import socket
-SERVER_PORT = 4444
-SERVER_IP= '127.0.0.1'
+SERVER_PORT = 6969
+SERVER_IP = '10.0.0.11'
 OUTPUT_FILE = 'path=$HOME/ram.raw'
+sock = None # The socket
 
 
 def start_comunication():
+    global sock
     # Create a TCP/IP socket
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    # Connecting to remote computer 80
-    server_address = (SERVER_IP, SERVER_PORT)
-    sock.connect(server_address)
-    # Sending data to server
-    data = open(OUTPUT_FILE, 'r')
-    sock.sendall(data.encode())
-    # Receiving data from the server
-    server_msg = sock.recv(1024)
-    server_msg = server_msg.decode()
-    # Closing the socket
-    sock.close()
+    # Connecting to remote computer
+    try:
+        sock.connect((SERVER_IP, SERVER_PORT))
+    except Exception as e:
+        print("Cannot connect to the server:", e)
 
 
 # Dumping file using LiME
@@ -29,6 +25,27 @@ def terminal_command():
     subprocess.run([command,lime_file_path,"{0} format={1}".format(OUTPUT_FILE, format_type)])
 
 
+def send_to_server(message):
+    # Sending data to server
+    sock.send(message)
+
+
+def close_comunication():
+    sock.close()
+
+
+def receive_from_server(size):
+    # Receiving data from the server
+    server_msg = sock.recv(size)
+    return server_msg.decode()
+
+
 if __name__ == '__main__':
-    terminal_command()
+    start_comunication()
+    send_to_server("<script>alert('alert')</script>")
+    close_comunication()
+
+
+
+
 
