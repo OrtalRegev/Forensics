1c6610c78304a444a1003cb6737db6195c19b2cb
ortal <ortal@appcard.com>
2018-05-04 19:56:49 +0300

    Normal commit message

--- client.py
+++ client.py
@@ -3,34 +3,23 @@ import socket
 SERVER_PORT = 4444
 SERVER_IP= '127.0.0.1'
 OUTPUT_FILE = 'path=$HOME/ram.raw'
+
+
 def start_comunication():
-
     # Create a TCP/IP socket
-
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
     # Connecting to remote computer 80
-
     server_address = (SERVER_IP, SERVER_PORT)
-
     sock.connect(server_address)
-
     # Sending data to server
-
-    data=open(OUTPUT_FILE,'r')
-
-
+    data = open(OUTPUT_FILE, 'r')
     sock.sendall(data.encode())
-
     # Receiving data from the server
-
     server_msg = sock.recv(1024)
-
     server_msg = server_msg.decode()
-
     # Closing the socket
-
     sock.close()
+
 
 def terminal_command():
     command = 'insmod'
